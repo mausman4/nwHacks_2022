c95a97dd50366b428b857646b705196b045aca85
SheaJanke <sheajanke@gmail.com>
2022-01-15 22:01:00 -0800

    moved to zeme 2.

--- zeme2/backend/server.js
+++ zeme2/backend/server.js
@@ -8,6 +8,6 @@ app.use(cors())
 app.use(express.json())
 
 app.use("/", zeme)
-app.use("*", (req, res) => res.status(404).json({error: "not found"}))
+app.use("*", (req, res) => res.status(404).json({error: "nothing found"}))
 
 export default app
